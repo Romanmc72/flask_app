8a58e1e73ca7acba75933209fd41b93e0e129792
Roman Czerwinski <roman.czerwinski@zoro.com>
2019-10-12 22:23:23 -0500

    making the deploy script a little friendlier for me when I run it

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 echo 'Shelling into remote server'
 ssh pi@$(nslookup r0m4n.hopto.org | grep -v '#' | awk '/Address/ { print $2 }')
+echo 'Repositioning to Flask Application repository'
+cd /var/www/flask_app
+echo 'Pulling down master branch updates'
+git pull
+echo 'All Done!'
 echo 'Exiting, Goodbye!'
 exit
+echo 'All Done!'
 echo 'Exited, Goodbye!'
