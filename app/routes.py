fa6389af11e37a280584cd3773826c927604d18d
Roman Czerwinski <roman.czerwinski@zoro.com>
2019-10-16 06:08:08 -0500

    added form validation and error messaging

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,10 @@ different pages on the Flask site,
 and how those html templates are populated.
 """
 from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
+
 from app import app
 from app.forms import LoginForm
 
@@ -27,10 +31,13 @@ def index():
         list_of_things=list_of_things
     )
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     """
     # TODO DOCUMENT ME
     """
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for {form.username.data}, remember me = {form.remember_me.data}")
+        return redirect(url_for('index'))
     return render_template("login.html", title='Login', form=form)
