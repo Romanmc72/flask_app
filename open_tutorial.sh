0c02d2a8d9293d94a012315db88089be773d68ec
Roman Czerwinski <roman.czerwinski@zoro.com>
2019-10-21 05:57:11 -0500

    moved tutorial reference to page 1

--- open_tutorial.sh
+++ open_tutorial.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Opening the tutorial in your browser"
-open https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates
+open https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
